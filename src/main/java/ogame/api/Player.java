66e088bbbc34f17d45c3824524b560b9d8bba1a0
Karol Kowalski <ordon1313@gmail.com>
2016-05-03 23:11:40 +0200

    Initial

---
+++ src/main/java/ogame/api/Player.java
@@ -0,0 +1,44 @@
+package ogame.api;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    protected Player() {}
+
+    public Player(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Player[id=%d, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+
+}
+
---
+++ src/main/java/ogame/api/PlayerRepository.java
@@ -0,0 +1,10 @@
+package ogame.api;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PlayerRepository extends CrudRepository<Player, Long> {
+
+    List<Player> findByLastName(String lastName);
+}
