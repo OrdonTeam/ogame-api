37102d7c66faf2d573337d8472d35452aa24eef0
Karol Kowalski <ordon1313@gmail.com>
2016-05-04 00:27:13 +0200

    Post

--- src/main/java/ogame/api/PlayerController.java
+++ src/main/java/ogame/api/PlayerController.java
@@ -1,7 +1,10 @@
 package ogame.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,8 +13,13 @@ public class PlayerController {
     @Autowired
     PlayerRepository playerRepository;
 
-    @RequestMapping("/players")
+    @RequestMapping(value = "/players", method = RequestMethod.GET)
     public Iterable<Player> players() {
         return playerRepository.findAll();
+    }
+
+    @RequestMapping(value = "/players", method = RequestMethod.POST)
+    public Player create(@RequestBody Player player) {
+        return playerRepository.save(player);
     }
 }
